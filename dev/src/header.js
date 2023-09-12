64f036b46d34afd6904565879de20b8936859975
SlavaFlava <parxomenko.90@inbox.ru>
2023-09-12 15:25:29 +0400

    started sticky header

--- dev/src/header.js
+++ dev/src/header.js
@@ -1,13 +1,37 @@
 if(!customElements.get('s-header')){
+
   customElements.define('s-header', class SHeader extends HTMLElement{
   constructor(){
     super();
   }
 
   connectedCallback(){
-    this.querySelector('.header').addEventListener("click", () => ('.header').classList.add('active'))
+  this.mobMenu()
+  this.stickyHeader()
   }
 
+  mobMenu() {
+        this.querySelector('.s-header__menu-btn').
+    addEventListener(
+      "click", () => (
+        this.classList.toggle('s-header--menu-active'),
+     document.querySelector('body').classList.toggle('hidden')
+     )
+     )
+  }
+
+  stickyHeader() {
+     console.log(this.getAttribute('data-sticky'));
+   function addSticky(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 200){
+      this.classList.add('sticky')
+    }
+   }
+
+     window.addEventListener('scroll', () =>addSticky())
+  }
+  
 })
 
 }
